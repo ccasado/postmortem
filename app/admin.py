943e7e4d46f68cbdc1fc30cf6910be779953c154
Cristiano Casado <ccasado@corp.globo.com>
2018-03-06 11:59:46 -0300

    New fields and style

--- app/admin.py
+++ app/admin.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.contrib import admin
 from .models import Report, Action
 
+
 admin.site.register(Report)
 admin.site.register(Action)
 
--- app/models.py
+++ app/models.py
@@ -3,18 +3,18 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
 
 class Report(models.Model):
     date = models.DateField('Date')
     pub_date = models.DateTimeField('Date published')
     product = models.CharField(max_length=50, default='', verbose_name='Product')
+    authors = models.CharField(max_length=100, default='', verbose_name='Authors')
     summary = models.CharField(max_length=100, default='', verbose_name='Summary')
-    impact = models.TextField('Impact', default='')
-    root_causes = models.TextField('Root causes', default='')
-    trigger = models.CharField(max_length=100, default='', verbose_name='Trigger')
+    impact = models.TextField('Impact', default='', help_text='Efeito sobre o usuário final ou ao negócio.')
+    root_causes = models.TextField('Root causes', default='', help_text='Explicação sobre as circunstâncias em que o incidente ocorreu. Pode ser útil usar a técnica 5 Whys.')
+    trigger = models.CharField(max_length=100, default='', verbose_name='Trigger', blank=True)
     resolution = models.TextField('Resolution', default='')
-    detection = models.CharField(max_length=100, default='', verbose_name='Detection')
+    detection = models.CharField(max_length=100, default='', verbose_name='Detection', blank=True)
 
     def __str__(self):
         return self.summary
@@ -25,14 +25,20 @@ class Action(models.Model):
     PROCESS = 'PROC'
     OTHER = 'OTH'
     ACTION_TYPE_CHOICES = (
-        (MITIGATE, 'mitigate'),
-        (PREVENT, 'prevent'),
-        (PROCESS, 'process'),
-        (OTHER, 'other')
+        (MITIGATE, 'mitigação'),
+        (PREVENT, 'prevenção'),
+        (PROCESS, 'processo'),
+        (OTHER, 'outro')
     )
     report = models.ForeignKey(Report, on_delete=models.CASCADE, default='')
     item = models.CharField(max_length=100, default='', verbose_name='Item')
     action_type = models.CharField(max_length=50, choices=ACTION_TYPE_CHOICES, default='other', verbose_name='Type')
+    owner = models.CharField(max_length=50, default='', verbose_name='Owner')
 
-    def __str__(self):
+    #def __str__(self):
+        #return self.item
+        #return unicode(self.item, 'utf-8')
+
+    def __unicode__(self):
         return self.item
+
