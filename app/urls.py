6210c554ba23241ee9dcf2414d78fc4ee93c2fff
Cristiano Casado <ccasado@corp.globo.com>
2018-03-06 18:27:30 -0300

    healtcheck as template

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-
+from django.views.generic.base import TemplateView
 from . import views
 
 urlpatterns = [
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     # ex: /report/2/
     url(r'^incidente/(?P<report_id>[0-9]+)/$', views.detail, name='detail'), 
+    url(r'^healthcheck.html$', TemplateView.as_view(template_name='healthcheck.html'), name='healthcheck'),
 ]
