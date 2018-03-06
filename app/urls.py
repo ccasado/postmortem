d1509e20682e2eee12c41d9f2008d54e0598579a
Cristiano Casado <ccasado@corp.globo.com>
2018-03-06 17:19:25 -0300

    New url mapping

--- app/urls.py
+++ app/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     # ex: /report/
     url(r'^$', views.index, name='index'),
     # ex: /report/2/
-    url(r'^(?P<report_id>[0-9]+)/$', views.detail, name='detail'), 
+    url(r'^incidente/(?P<report_id>[0-9]+)/$', views.detail, name='detail'), 
 ]
