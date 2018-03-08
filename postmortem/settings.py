b5402c446ab2241e2b40a7649a28887db14a08ce
Cristiano Casado <ccasado@corp.globo.com>
2018-03-08 12:25:31 -0300

    debug false

--- postmortem/settings.py
+++ postmortem/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'j9lowy69kcygc0v=s4)3-uvo-m!#teb1_*4j2o77--l7ob=pk2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'localhost',
@@ -74,15 +74,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'postmortem.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
 
 DATABASES = {
     'default': {
