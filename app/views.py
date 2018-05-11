26f4d9a0eb2197820520dbefb82e1e6fc711c93d
Cristiano Casado <ccasado@corp.globo.com>
2018-05-11 13:44:41 -0300

    changing the order to date

--- app/views.py
+++ app/views.py
@@ -2,12 +2,13 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 from django.views.decorators.cache import cache_page
-from .models import Report
-from .filters import ReportFilter
+from django.db.models import Count
+from .models import Report, Action
+from .filters import ReportFilter, ActionFilter
 
 @cache_page(60 * 1)
 def index(request):
-    report_list = Report.objects.order_by('-pub_date')
+    report_list = Report.objects.order_by('date')
     report_filter = ReportFilter(request.GET, queryset=report_list)
     return render(request, 'reports/index.html', {'filter': report_filter})
 
@@ -15,3 +16,14 @@ def index(request):
 def detail(request, report_id):
     report = get_object_or_404(Report, pk=report_id)
     return render(request, 'reports/detail.html', {'report':report})
+
+def actions(request):
+    action_list = Action.objects.order_by('-pub_date')
+    action_filter = ActionFilter(request.GET, queryset=action_list)
+    return render(request, 'reports/actions.html', {'filter': action_filter})
+
+def numbers(request):
+	total = Report.objects.count()
+	p = Report.objects.values('product').annotate(total=Count('product')).order_by()
+	context = {'total': total, 'p': p}
+	return render(request, 'reports/numbers.html', context)
